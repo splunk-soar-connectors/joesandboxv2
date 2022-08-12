5ab7fe5f990401f4d969b88e075ee5ccf46118d1
Ishan Ghelani <ighelani@splunk.com>
2022-08-12 14:30:16 +0530

    JoeSandboxv2: PAPP-26954: Dev checklist changes

--- joesandboxv2_consts.py
+++ joesandboxv2_consts.py
@@ -12,13 +12,17 @@
 # the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
+
+# Config and action keys
 JOE_CONFIG_SERVER = 'url'
-JOE_CONFIG_API_KEY = 'api_key'
+JOE_CONFIG_API_KEY = 'api_key'  # pragma: allowlist secret
 JOE_CONFIG_TIMEOUT = 'timeout'
 JOE_CONFIG_VERIFY_SSL = 'verify_ssl'
 JOE_CONFIG_ANALYSIS_TIME = 'analysis_time'
 JOE_ACTION_GET_REPORT = 'get_report'
 JOE_ACTION_GET_PCAP = 'get_pcap'
+
+# Endpoints
 JOE_LIST_COOKBOOKS_ENDPOINT = '/api/v2/cookbook/list'
 JOE_GET_COOKBOOK_ENDPOINT = '/api/v2/cookbook/info'
 JOE_CHECK_STATUS_ENDPOINT = '/api/v2/analysis/info'
@@ -26,6 +30,9 @@ JOE_TEST_CONNECTIVITY_ENDPOINT = '/api/v2/server/online'
 JOE_DETONATE_SAMPLE_ENDPOINT = '/api/v2/analysis/submit'
 JOE_REPORT_ENDPOINT = '/api/v2/analysis/download'
 JOE_REPUTATION_SEARCH_ENDPOINT = '/api/v2/analysis/search'
+
+# Error and status messages
+JOE_ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
 JOE_URL_SEARCH_ANALYSIS_NOT_FOUND_MSG = 'No search analysis found for provided URL'
 JOE_HASH_SEARCH_ANALYSIS_NOT_FOUND_MSG = 'No search analysis found for provided hash'
 JOE_REPORT_ALREADY_AVAILABLE_MSG = 'The report is already available in vault'
@@ -47,7 +54,7 @@ JOE_ERR_API_UNSUPPORTED_METHOD_MSG = 'Unsupported method to make request'
 JOE_ERR_SERVER_CONNECTION_MSG = 'Connection failed'
 JOE_ERR_INVALID_URL_MSG = 'Status Code: {status_code}. Please provide a valid base URL in configuration parameters. The error is: {error_msg}'
 JOE_ERR_FROM_SERVER_MSG = 'API failed\nStatus code: {status}\nReason: {reason}\n'
-JOE_ERR_UNKNOWN_ERROR_MSG = 'Unknown error occurred'
+JOE_ERR_UNKNOWN_ERR_MSG = 'Unknown error occurred'
 JOE_ERR_CHECK_STATUS_MSG = 'Failed while checking status of sample submitted'
 JOE_ERR_JSON_MSG = 'Error while converting content data in json format: {error}'
 JOE_ERR_FILE_MSG = 'Error while creating file'
@@ -56,8 +63,10 @@ JOE_PCAP_REPORT_DOWNLOAD_MSG = 'PCAP report downloaded successfully'
 JOE_ERR_ADDING_TO_VAULT_FAILED_MSG = 'Error occurred while adding the file to vault'
 JOE_ERR_FILE_OR_COOKBOOK_NOT_FOUND_MSG = 'Either the file or the cookbook for the provided hash not found in vault'
 JOE_ERR_REPORT_FILENAME_NOT_FOUND_MSG = 'Incorrect response format. Not able to find "Content-Disposition" in headers'
+
+# JSON keys
 JOE_JSON_RESPONSE = 'response'
-JOE_JSON_API_KEY = 'apikey'
+JOE_JSON_API_KEY = 'apikey'  # pragma: allowlist secret
 JOE_JSON_RESPONSE_HEADERS = 'headers'
 JOE_JSON_DATA = 'data'
 JOE_JSON_OR = ' or '
@@ -75,7 +84,6 @@ JOE_JSON_URL = 'url'
 JOE_JSON_HASH = 'hash'
 JOE_JSON_FILE_VAULT_ID = 'vault_id'
 JOE_JSON_ERRORS = 'errors'
-JOE_JSON_OR = ' or '
 JOE_JSON_MESSAGE = 'message'
 JOE_JSON_ANALYSIS = 'analysis'
 JOE_JSON_COOKBOOK = 'cookbook'
@@ -94,7 +102,6 @@ JOE_JSON_IP = 'ip'
 JOE_JSON_SIGNATURED_DETECTIONS = 'signaturedetections'
 JOE_JSON_STRATEGY = 'strategy'
 JOE_JSON_DROPPED_INFO = 'droppedinfo'
-JOE_JSON_HASH = 'hash'
 JOE_JSON_FILE_ACTIVITIES = 'fileactivities'
 JOE_JSON_FILE_DELETED = 'fileDeleted'
 JOE_JSON_FILE_MOVED = 'fileMoved'
@@ -110,11 +117,9 @@ JOE_JSON_REPORT_FILE_NAME = 'report_file_name'
 JOE_JSON_SHA_256 = 'sha256'
 JOE_JSON_SHA_1 = 'sha1'
 JOE_JSON_MD_5 = 'md5'
-JOE_JSON_WEBID = 'webid'
 JOE_JSON_GENERAL_INFO = 'generalinfo'
 JOE_JSON_FILE_INFO = 'fileinfo'
 JOE_JSON_ANALYSIS_STATUS = 'analysis_status'
-JOE_JSON_ANALYSIS = 'analysis'
 JOE_JSON_BEHAVIOR = 'behavior'
 JOE_JSON_PROCESSES = 'processes'
 JOE_JSON_PROCESS = 'process'
@@ -133,11 +138,11 @@ JOE_JSON = 'json'
 JOE_JSON_SIZE = 'size'
 JOE_JSON_TARGET = 'target'
 JOE_JSON_FILE_NAME = 'cookbook_file_name'
-JOE_JSON_REPORT_FILE_NAME = 'report_file_name'
-JOE_JSON_COOKBOOK_VAULT_ID = 'cookbook_vault_id'
 JOE_JSON_VAULT_ID = 'vault_id'
 JOE_JSON_CONTENT_DISPOSITION = 'Content-Disposition'
 JOE_JSON_RESP_SUCCESS_RESPONSE = 200
+
+# Default values
 JOE_TIME_DEFAULT = '120'
 JOE_TIME_MIN = 30
 JOE_TIME_MAX = 300
